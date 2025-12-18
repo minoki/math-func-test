738b4434e0b8a1b837c5d2877c46a329c1ca2c4a
ARATA Mizuki <minorinoki@gmail.com>
2025-12-18 22:50:30 +0900

    Update sample-mini

--- sample-mini.c
+++ sample-mini.c
@@ -7,24 +7,24 @@ int main(void)
         volatile double x = 0x1p2;
         double y = sin(x);
         double cr_result = -0x1.837b9dddc1eaep-1;
-        printf("sin(%.17g [%a]) = %.17g [%a]", x, x, y, y);
+        printf("sin(%.16g [%a]) = %.16g [%a]", x, x, y, y);
         if (y == cr_result) {
             puts("");
         } else {
             // AArch64 macOS
-            printf("* (cr: %.17g [%a])\n", cr_result, cr_result);
+            printf("* (cr: %.16g [%a])\n", cr_result, cr_result);
         }
     }
     {
         volatile double x = -0x1.40a3d70a3d70ap+2;
         double y = exp(x);
         double cr_result = 0x1.b52f2f88ff3ddp-8;
-        printf("exp(%.17g [%a]) = %.17g [%a]", x, x, y, y);
+        printf("exp(%.16g [%a]) = %.16g [%a]", x, x, y, y);
         if (y == cr_result) {
             puts("");
         } else {
             // x86-64 macOS
-            printf("* (cr: %.17g [%a])\n", cr_result, cr_result);
+            printf("* (cr: %.16g [%a])\n", cr_result, cr_result);
         }
     }
     {
@@ -32,12 +32,12 @@ int main(void)
         volatile double y = -0x1.599999999999ap+1;
         double cr_result = 0x1.f618446e34cf5p-7;
         double z = pow(x, y);
-        printf("pow(%.17g [%a], %.17g [%a]) = %.17g [%a]", x, x, y, y, z, z);
+        printf("pow(%.16g [%a], %.16g [%a]) = %.16g [%a]", x, x, y, y, z, z);
         if (z == cr_result) {
             puts("");
         } else {
             // x86-64 UCRT
-            printf("* (cr: %.17g [%a])\n", cr_result, cr_result);
+            printf("* (cr: %.16g [%a])\n", cr_result, cr_result);
         }
     }
     {
@@ -45,48 +45,48 @@ int main(void)
         volatile double y = -0x1.999999999999ap-4;
         double cr_result = 0x1.0d6aba2f6f6a6p+0;
         double z = pow(x, y);
-        printf("pow(%.17g [%a], %.17g [%a]) = %.17g [%a]", x, x, y, y, z, z);
+        printf("pow(%.16g [%a], %.16g [%a]) = %.16g [%a]", x, x, y, y, z, z);
         if (z == cr_result) {
             puts("");
         } else {
             // x86-64/AArch64 glibc
-            printf("* (cr: %.17g [%a])\n", cr_result, cr_result);
+            printf("* (cr: %.16g [%a])\n", cr_result, cr_result);
         }
     }
     {
         volatile double x = 0x1.135c28f5c28f6p+4;
         double cr_result = 0x1.282f212d7731ap+8;
         double y = pow(x, 2.0);
-        printf("pow(%.17g [%a], 2.0) = %.17g [%a]", x, x, y, y);
+        printf("pow(%.16g [%a], 2.0) = %.16g [%a]", x, x, y, y);
         if (y == cr_result) {
             puts("");
         } else {
             // x86-64/AArch64 macOS
-            printf("* (cr: %.17g [%a])\n", cr_result, cr_result);
+            printf("* (cr: %.16g [%a])\n", cr_result, cr_result);
         }
     }
     {
         volatile double x = 0x1.7fe147ae147aep+6;
         double cr_result = 0x1.1fd1ed5cfaacdp+13;
         double y = pow(x, 2.0);
-        printf("pow(%.17g [%a], 2.0) = %.17g [%a]", x, x, y, y);
+        printf("pow(%.16g [%a], 2.0) = %.16g [%a]", x, x, y, y);
         if (y == cr_result) {
             puts("");
         } else {
             // x86-64/AArch64 glibc
-            printf("* (cr: %.17g [%a])\n", cr_result, cr_result);
+            printf("* (cr: %.16g [%a])\n", cr_result, cr_result);
         }
     }
     {
         volatile double x = 0x1.2eccccccccccdp+6;
         double cr_result = 0x1.166b1b9eb5f04p+3;
         double y = pow(x, 0.5);
-        printf("pow(%.17g [%a], 0.5) = %.17g [%a]", x, x, y, y);
+        printf("pow(%.16g [%a], 0.5) = %.16g [%a]", x, x, y, y);
         if (y == cr_result) {
             puts("");
         } else {
             // x86-64/AArch64 musl
-            printf("* (cr: %.17g [%a])\n", cr_result, cr_result);
+            printf("* (cr: %.16g [%a])\n", cr_result, cr_result);
         }
     }
 }

4fdd8df65d585d47c72211ef8975cbd5f3915402
ARATA Mizuki <minorinoki@gmail.com>
2025-12-18 22:28:53 +0900

    Update const-sin.c

--- const-sin.c
+++ const-sin.c
@@ -6,6 +6,6 @@ int main()
     constexpr double a = sin(0x1p938);
     const volatile double x = 0x1p938;
     double b = sin(x);
-    printf("sin[compile-time](0x1p938)=%.17g [%a]\n", a, a);
-    printf("sin[runtime](0x1p938)=%.17g [%a]\n", b, b);
+    printf("[compile-time] sin(0x1p938)=%.17g [%a]\n", a, a);
+    printf("[runtime] sin(0x1p938)=%.17g [%a]\n", b, b);
 }
